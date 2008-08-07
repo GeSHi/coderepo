ec16d0265983e171a8f327b6fc4adb56e3f6efdc
Milian Wolff <mail@milianw.de>
2008-08-07 19:45:26 +0000

    add: number testcase

---
+++ php/php_numbers.php
@@ -0,0 +1,18 @@
+<?php
+// some integers
+$foo = array(42, 123,123568, 123);
+$bar = -123;
+$bar += 1-2/4123;
+
+// some floats
+$zark = .123;
+$foo = 0.123;
+$bar = 4.2e3;
+$bof = 4e1234;
+$neg = -.12E-15;
+
+// some octals
+$foo = array(0777, 0000, 0123);
+
+// some hexs
+$bar = array(0xff, 0x00, 0xbf, 0x1b, 0x12);
