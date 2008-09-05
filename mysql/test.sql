e53ac4e1744713fc99bde022899231148df13686
Benny Baumann <benbe@geshi.org>
2008-09-05 17:39:20 +0000

    add: Extended the MySQL testcase a bit

--- mysql/test.sql
+++ mysql/test.sql
@@ -1,3 +1,10 @@
 ALTER TABLE `xcart_sessions_data` ADD COLUMN `last_updated` TIMESTAMP
 ON UPDATE CURRENT_TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP;
 ALTER TABLE `xcart_sessions_data` ADD INDEX `last_updated` (`last_updated`);
+
+-- Some standard SELECT ...
+SELECT *
+FROM `geshi_downloads`
+WHERE `download_id` = 1
+OR `download_id` = 2
+LIMIT 0, 1000
